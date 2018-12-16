13a0ed29351b0e5559148bb3a3d488cfdb92b4de
Matthias Clasen <mclasen@redhat.com>
2018-12-16 09:19:59 -0500

    Strip remote D-Bus errors
    Less goo.

--- src/flatpak-spawn.c
+++ src/flatpak-spawn.c
@@ -475,6 +475,7 @@ main (int    argc,
 
   if (reply == NULL)
     {
+      g_dbus_error_strip_remote_error (error);
       g_printerr ("Failed to call flatpak portal: %s\n", error->message);
       return 1;
     }
