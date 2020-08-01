flatpak/flatpak-xdg-utils

commit 54c5800fe064a33311e543f928ccc0620e2cef4e

spawn: fix propagating exit code
The exit status in the HostCommandExited/SpawnExited signal is as
returned by waitpid(), which is a combination of flags indicating the
way in which the process exited (or was SIGSTOPed or SIGCONTed) and the
exit code or signal number. The exact layout is platform-specific, but
on Linux the gist is:

* on exit(x), the exit status is ((x & 0xff) << 8); the low byte is 0
* on being killed by signal y, the exit status is in the low byte, with
  its high bit (0x80) set if the process dumped core

The number passed to exit() is specified to be masked with 0xff to fall
in the range [0, 255].

These are different numbers, so passing one where the other is expected
does not work:

* A process that calls exit(1) has exit status 256 (0x0100). Since
  (0x0100 & 0xff) == 0, passing this to exit() means flatpak-spawn will
  exit with success, despite its child failing.
* A process killed by SIGTERM 15 (0x0f) without dumping core has exit
  status 15 (0x000f).  Passing this to exit() means flatpak-spawn will
  exit with status 15, which is at least non-success but gives no
  indication that it was terminated.
* A process which segfaults (signal 11, 0x0b) and dumps core has exit
  status (0x0b | 0x80) == 0x8b (139). This happens to matches what the
  shell reports for a process killed by SIGSEGV, since the shell sets $?
  to (128 + signal number), but only because the "dumped core" flag is
  set.

So, we need to use the macros from sys/wait.h to inspect the exit
status and recover the exit code or signal. For normal termination, we
can pass the exit code to exit(). For signals, our options are:

* Reset all signal() handlers to SIG_DFL, then send the signal to
  ourselves and hope we die
* Follow the shell convention and exit(128 + signal number)

The latter is implemented here. It means that (for example)
g_spawn_check_exit_status() reports "g-spawn-exit-error-quark: Child
process exited with code 139 (139)" rather than "g-exec-error-quark:

src/flatpak-spawn.c
@@ -22,6 +22,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <sys/wait.h>
 #include <gio/gio.h>
 #include <gio/gunixfdlist.h>
 
@@ -64,7 +65,40 @@ spawn_exited_cb (GDBusConnection *connection,
   g_debug ("child exited %d: %d", client_pid, exit_status);
 
   if (child_pid == client_pid)
-    exit (exit_status);
+    {
+      int exit_code;
+
+      if (WIFEXITED (exit_status))
+        {
+          exit_code = WEXITSTATUS (exit_status);
+        }
+      else if (WIFSIGNALED (exit_status))
+        {
+          /* Smush the signal into an unsigned byte, as the shell does. This is
+           * not quite right from the perspective of whatever ran flatpak-spawn
+           * — it will get WIFEXITED() not WIFSIGNALED() — but the
+           *  alternative is to disconnect all signal() handlers then send this
+           *  signal to ourselves and hope it kills us.
+           */
+          exit_code = 128 + WTERMSIG (exit_status);
+        }
+      else
+        {
+          /* wait(3p) claims that if the waitpid() call that returned the exit
+           * code specified neither WUNTRACED nor WIFSIGNALED, then exactly one
+           * of WIFEXITED() or WIFSIGNALED() will be true.
+           */
+          g_warning ("exit status %d is neither WIFEXITED() nor WIFSIGNALED()",
+                     exit_status);
+          /* EX_SOFTWARE "internal software error" from sysexits.h, for want of
+           * a better code.
+           */
+          exit_status = 70;
+        }
+
+      g_debug ("child exit code %d: %d", client_pid, exit_code);
+      exit (exit_code);
+  }
 }
 
 static void

commit d6bd82bd4f2d3eaa08daac2cbc5dcd6f38739a5d

flatpak-spawn: Use exit code 70 for weird wait-statuses as intended
The original implementation mistakenly reset exit_status instead of
exit_code. This originally left exit_code undefined, with subsequent
commit 50991f5f "Fix (bogus) uninitialized use warning" changing the
undefined exit code to 0.

Revert 50991f5f, because the warning turned out not to have
been bogus, and should be silenced by this change.

Fixes: 54c5800f "spawn: fix propagating exit code"
Signed-off-by: Simon McVittie <smcv@collabora.com>

src/flatpak-spawn.c
@@ -88,7 +88,7 @@ spawn_exited_cb (G_GNUC_UNUSED GDBusConnection *connection,
 
   if (child_pid == client_pid)
     {
-      int exit_code = 0;
+      int exit_code;
 
       if (WIFEXITED (exit_status))
         {
@@ -115,7 +115,7 @@ spawn_exited_cb (G_GNUC_UNUSED GDBusConnection *connection,
           /* EX_SOFTWARE "internal software error" from sysexits.h, for want of
            * a better code.
            */
-          exit_status = 70;
+          exit_code = 70;
         }
 
       g_debug ("child exit code %d: %d", client_pid, exit_code);