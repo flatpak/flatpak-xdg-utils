flatpak/flatpak-xdg-utils

commit f8ed903c227b37110fc30bd1233d675f1c93c060

email: Pass attachments as fds
Accepting local files as URIs allows leaking resources from outside
the sandbox, so the Email portal will no longer permit this. Instead,
attachments need to be passed as file descriptors.

Fixes #3.

src/xdg-email.c
@@ -21,6 +21,11 @@
 #include <gio/gio.h>
 
 #include <glib/gi18n.h>
+#include <gio/gunixfdlist.h>
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #define PORTAL_BUS_NAME    "org.freedesktop.portal.Desktop"
 #define PORTAL_OBJECT_PATH "/org/freedesktop/portal/desktop"
@@ -80,6 +85,8 @@ main (int argc, char *argv[])
   GError *error = NULL;
   GDBusConnection *connection;
   GVariantBuilder opt_builder;
+  GVariant *parameters;
+  GUnixFDList *fd_list = NULL;
 
   context = g_option_context_new ("[ mailto-uri | address(es) ]");
 
@@ -142,23 +149,51 @@ main (int argc, char *argv[])
                            "body", g_variant_new_string (body));
 
   if (attach != NULL)
-    g_variant_builder_add (&opt_builder,
-                           "{sv}",
-                           "attachments", g_variant_new_parsed ("[%s]", attach));
+    {
+      GFile *file = g_file_new_for_commandline_arg (attach);
+      char *path;
+      int fd;
 
-  g_dbus_connection_call_sync (connection,
-                               PORTAL_BUS_NAME,
-                               PORTAL_OBJECT_PATH,
-                               PORTAL_IFACE_NAME,
-                               "ComposeEmail",
-                               g_variant_new ("(s@a{sv})",
-                                              "",
-                                              g_variant_builder_end (&opt_builder)),
-                               NULL,
-                               G_DBUS_CALL_FLAGS_NONE,
-                               -1,
-                               NULL,
-                               &error);
+      if (!g_file_is_native (file))
+        {
+          g_printerr ("Only native files can be used as attachments");
+          g_object_unref (file);
+          return 2;
+        }
+
+      path = g_file_get_path (file);
+      fd = open (path, O_PATH | O_CLOEXEC);
+      if (fd == -1)
+        {
+          g_printerr ("Failed to open '%s': %s", path, g_strerror (errno));
+          return 2;
+        }
+
+      fd_list = g_unix_fd_list_new_from_array (&fd, 1);
+      fd = -1;
+
+      g_variant_builder_add (&opt_builder,
+                             "{sv}",
+                             "attachments", g_variant_new_parsed ("[0]"));
+    }
+
+  parameters = g_variant_new ("(s@a{sv})",
+                              "",
+                              g_variant_builder_end (&opt_builder));
+
+  g_dbus_connection_call_with_unix_fd_list_sync (connection,
+                                                 PORTAL_BUS_NAME,
+                                                 PORTAL_OBJECT_PATH,
+                                                 PORTAL_IFACE_NAME,
+                                                 "ComposeEmail",
+                                                 parameters,
+                                                 NULL,
+                                                 G_DBUS_CALL_FLAGS_NONE,
+                                                 -1,
+                                                 fd_list,
+                                                 NULL,
+                                                 NULL,
+                                                 &error);
 
   if (error)
     {

commit 18e28bc6051127ca6eb8bdc021c215768c3b69c4

xdg-email: Send attachments using the correct option name and type
xdg-desktop-portal 0.9 switched from attachments as an array of URIs to
attachment_fds as an array of fds, but flatpak-xdg-utils 1.0.0 switched
from attachments as an array of URIs to attachments as an array of ints
(not fds!), resulting in failure to interoperate.

Fixes: f8ed903c "email: Pass attachments as fds"
Resolves: #50
Signed-off-by: Simon McVittie <smcv@collabora.com>

src/xdg-email.c
@@ -385,7 +385,7 @@ main (int argc, char *argv[])
 
       g_variant_builder_add (&opt_builder,
                              "{sv}",
-                             "attachments", g_variant_new_parsed ("[0]"));
+                             "attachment_fds", g_variant_new_parsed ("@ah [0]"));
     }
 
   parameters = g_variant_new ("(s@a{sv})",